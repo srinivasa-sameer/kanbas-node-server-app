f2d68f3fbe019ec07b19abd78ad5ad391db27d0d
Srinivasa Sameer Addepalli <srinivasaddepalli13@gmail.com>
2023-11-13 20:50:12 -0500

    A5 Updates

--- Lab5.js
+++ Lab5.js
@@ -15,23 +15,26 @@ const todos = [
 ];
 
 const Lab5 = (app) => {
-  app.get('/a5/todos', (req, res) => {
-    const { completed } = req.query;
-    if (completed !== undefined) {
-      const completedTodos = todos.filter((t) => t.completed === completed);
-      res.json(completedTodos);
-      return;
-    }
+  app.post('/a5/todos', (req, res) => {
+    const newTodo = {
+      ...req.body,
+      id: new Date().getTime(),
+    };
+    todos.push(newTodo);
     res.json(todos);
   });
 
   app.get('/a5/todos/:id', (req, res) => {
     const { id } = req.params;
-    const todo = todos.find((t) => t.id === parseInt(id));
+    const todo = todos.find((todo) => todo.id === parseInt(id));
+    if (!todo) {
+      res.status(404).send('Todo not found');
+      return;
+    }
     res.json(todo);
   });
 
-  app.get('/a5/todos/create', (req, res) => {
+  app.post('/a5/todos/create', (req, res) => {
     const newTodo = {
       id: new Date().getTime(),
       title: 'New Task',
@@ -41,17 +44,25 @@ const Lab5 = (app) => {
     res.json(todos);
   });
 
-  app.get('/a5/todos/:id/delete', (req, res) => {
+  app.delete('/a5/todos/:id/delete', (req, res) => {
     const { id } = req.params;
-    const todo = todos.find((t) => t.id === parseInt(id));
-    todos.splice(todos.indexOf(todo), 1);
+    const index = todos.findIndex((todo) => todo.id === parseInt(id));
+    if (index === -1) {
+      res.status(404).send('Todo not found');
+      return;
+    }
+    todos.splice(index, 1);
     res.json(todos);
   });
 
-  app.get('/a5/todos/:id/title/:title', (req, res) => {
-    const { id, title } = req.params;
-    const todo = todos.find((t) => t.id === parseInt(id));
-    todo.title = title;
+  app.get('/a5/todos/:id/title/:newTitle', (req, res) => {
+    const { id, newTitle } = req.params;
+    const todo = todos.find((todo) => todo.id === parseInt(id));
+    if (!todo) {
+      res.status(404).send('Todo not found');
+      return;
+    }
+    todo.title = newTitle;
     res.json(todos);
   });
 
