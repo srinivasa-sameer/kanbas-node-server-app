1c8f713a25bd258cb9be9c64549cd64d4f9395e1
Srinivasa Sameer Addepalli <srinivasaddepalli13@gmail.com>
2023-11-19 20:49:38 -0500

    Node A5

---
+++ Database/index.js
@@ -0,0 +1,11 @@
+import courses from './courses.json' assert { type: 'json' };
+import links from './links.json' assert { type: 'json' };
+import modules from './modules.json' assert { type: 'json' };
+import assignments from './assignments.json' assert { type: 'json' };
+
+export default {
+  courses,
+  links,
+  modules,
+  assignments,
+};
--- app.js
+++ app.js
@@ -2,10 +2,15 @@ import express from 'express';
 import Hello from './hello.js';
 import Lab5 from './Lab5.js';
 import cors from 'cors';
+import 'dotenv/config';
+import CourseRoutes from './Courses/routes.js';
+import ModuleRoutes from './Modules/routes.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 Hello(app);
 Lab5(app);
-app.listen(4000);
+CourseRoutes(app);
+ModuleRoutes(app);
+app.listen(process.env.PORT || 4000);
